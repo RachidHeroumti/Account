cdeba7a167104077d9f5ff0c9198b4c5cd8040bd
rachidcooder <cooderherou@gmail.com>
2025-01-06 16:00:22 +0100

    set up project

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+   <div style="color: aqua;" class="bg-red-500">hhhhhhhhhhhhhh</div> 
+</template> 
+<script>
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>mainnnnnn </div>
+</template>
